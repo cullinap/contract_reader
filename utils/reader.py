7b6726fdf8e282792fe1b3a6ff1bc9ba0f50f605
Patrick-Cullinane <patrick.cullinane@ibm.com>
2020-01-21 07:21:16 -0500

    finished reader

--- utils/reader.py
+++ utils/reader.py
@@ -1,11 +1,15 @@
+import re
+import os
+import pandas as pd 
 import pandas as pd 
 import numpy as np 
-from process_data import preprocess #?
+from tika import parser
+from utils.process_data import preprocess
+from sklearn.externals import joblib
 
-model='./data/document_prediction_model.sav'
+model = joblib.load('./data/document_prediction_model.sav')
 
-
-def read(file, model=):
+def read(file, path, model=model):
     '''
     goes to each folder and iterates through the pdf files in a folder
     takes:
@@ -13,23 +17,27 @@ def read(file, model=):
     -path: path to the data folders
 
     '''
+    process_val = {'stopwords':'english',
+               'custom_sw':'aaa',
+               'stemmer':'yes',
+               'ngrams':'bigrams'}
+    
     regex = '\n\n(?=\u2028|[A-Z-0-9])' #removed one \n
 
-    try:
 
-        clean_file = re.split(regex, 
-            [parser.from_file(path + file + '/' + doc) for doc in os.listdir(path + file)] 
-         [0]['content'].strip()
-        )
+    clean_file = re.split(regex, 
+                    [parser.from_file(path + file)]
+     [0]['content'].strip()
+    )
+    
+        
+    prep = lambda i: preprocess(pd.Series(i),**process_val).apply(lambda x:', '.join(map(str, x)))
+    
+    preprocessed = prep(clean_file)
 
-        prep = lambda i: preprocess(pd.Series(i),**process_val).apply(lambda x:', '.join(map(str, x)))
+    labels = list(model.predict(preprocessed))
 
-        labels = [model.predict(prep(x))[0] for x in new_data['1st Century Bank']]
-        text = [i[:300] for i in new_data['1st Century Bank']]
+    text = [i[:300] for i in clean_file]
 
-        return df = pd.DataFrame({'label':labels, 'text': text})
-
-
-    except:
-        print('no data')
-        return ['no data']
+    return pd.DataFrame({'label':labels, 'text': text})
+   
