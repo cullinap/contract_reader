f2f7372d8b0a1fc50193b4ad654186e81e5ae750
Patrick-Cullinane <patrick.cullinane@ibm.com>
2020-01-23 10:21:52 -0500

    update setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="contract_reader",
+    version="1.0",
+    description="A useful module",
+    author="Patrick Cullinane",
+    author_email="",
+    packages=["contract_reader"],  # same as name
+)
