c1e079122d83564bc149b16c7a04321643d4de88
cullinap <cullinap@gmail.com>
2020-02-02 12:53:20 -0500

    add slides

--- utils/shelf.py
+++ utils/shelf.py
@@ -1,5 +1,10 @@
 #old code:
 
+
+
+
+
+
 documents = {f: re.split('\n\n(?=\u2028|[A-Z-0-9])',
                 [parser.from_file(DATA_PATH + f + '/' + doc) for doc in os.listdir(DATA_PATH + f)]
             [0]['content'].strip()
